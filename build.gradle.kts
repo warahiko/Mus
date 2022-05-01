fe11128e82caa7414676f7cbdd9be05047518b04
Go Shibata <warahiko@gmail.com>
2022-05-01 22:20:45 +0900

    build.gradle をkts に変更

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "7.1.3" apply false
+    id("com.android.library") version "7.1.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+subprojects {
+    val ktlint by configurations.creating
+
+    dependencies {
+        ktlint("com.pinterest:ktlint:0.45.2") {
+            attributes {
+                attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+            }
+        }
+    }
+
+    val outputDir = "${project.buildDir}/reports/ktlint/"
+    val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+
+    val ktlintCheck by tasks.creating(JavaExec::class) {
+        inputs.files(inputFiles)
+        outputs.dir(outputDir)
+
+        description = "Check Kotlin code style."
+        classpath = ktlint
+        mainClass.set("com.pinterest.ktlint.Main")
+        args = listOf(
+            "--android",
+            "--color",
+            "--reporter=plain",
+            "--reporter=checkstyle,output=$buildDir/reports/ktlint-results.xml",
+            "src/**/*.kt",
+        )
+        isIgnoreExitValue = true
+    }
+
+    val ktlintFormat by tasks.creating(JavaExec::class) {
+        inputs.files(inputFiles)
+        outputs.dir(outputDir)
+
+        description = "Fix Kotlin code style deviations."
+        classpath = ktlint
+        mainClass.set("com.pinterest.ktlint.Main")
+        args = listOf("-F", "--android", "src/**/*.kt")
+        isIgnoreExitValue = true
+    }
+}
