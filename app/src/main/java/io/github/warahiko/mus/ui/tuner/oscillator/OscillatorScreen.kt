b01f2f6b81f00f20d365e4bb66ffee5a5e15c5f2
Go Shibata <warahiko@gmail.com>
2022-05-06 14:10:22 +0900

    oscillator 画面の仮実装

--- app/src/main/java/io/github/warahiko/mus/ui/tuner/oscillator/OscillatorScreen.kt
+++ app/src/main/java/io/github/warahiko/mus/ui/tuner/oscillator/OscillatorScreen.kt
@@ -1,40 +1,74 @@
 package io.github.warahiko.mus.ui.tuner.oscillator
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.warahiko.mus.R
 import io.github.warahiko.mus.ui.component.MusTopBar
 import io.github.warahiko.mus.ui.theme.MusAppTheme
+import io.github.warahiko.mus.ui.tuner.oscillator.notename.NoteName
+import io.github.warahiko.mus.ui.tuner.oscillator.section.NoteNameSection
+import io.github.warahiko.mus.ui.tuner.oscillator.section.PitchSection
 
 @Composable
 fun OscillatorScreen(
     modifier: Modifier = Modifier,
 ) {
+    val (selectedNoteName, setNoteName) = remember { mutableStateOf(NoteName.C) }
+    val (octave, setOctave) = remember { mutableStateOf(4f) }
+    val (a4Frequency, setA4Frequency) = remember { mutableStateOf(442f) }
+
+    val scrollState = rememberScrollState()
     Scaffold(
         modifier = modifier,
         topBar = {
             MusTopBar(
                 title = stringResource(id = R.string.oscillator_title),
             )
+        },
+        floatingActionButton = {
+            LargeFloatingActionButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_play),
+                    contentDescription = null,
+                )
+            }
         }
     ) { contentPadding ->
-        Box(
+        Column(
             modifier = Modifier
                 .padding(contentPadding)
-                .fillMaxSize(),
+                .padding(horizontal = 20.dp)
+                .padding(top = 12.dp, bottom = 24.dp)
+                .fillMaxSize()
+                .verticalScroll(scrollState),
         ) {
-            Text(
-                text = "Oscillator",
-                modifier = Modifier.align(Alignment.Center),
+            NoteNameSection(
+                selectedNoteName = selectedNoteName,
+                onClickButton = setNoteName
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            PitchSection(
+                octave = octave,
+                a4Frequency = a4Frequency,
+                onChangeOctave = setOctave,
+                onChangeA4Frequency = setA4Frequency,
             )
         }
     }
---
+++ app/src/main/java/io/github/warahiko/mus/ui/tuner/oscillator/section/NoteNameSction.kt
@@ -0,0 +1,77 @@
+package io.github.warahiko.mus.ui.tuner.oscillator.section
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.warahiko.mus.ui.theme.MusAppTheme
+import io.github.warahiko.mus.ui.tuner.oscillator.notename.NoteName
+
+@Composable
+fun NoteNameSection(
+    selectedNoteName: NoteName,
+    modifier: Modifier = Modifier,
+    onClickButton: (noteName: NoteName) -> Unit = {},
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.CenterStart,
+            ) {
+                Text(text = "音名")
+            }
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(100.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(max = (64 * NoteName.values().size).dp),
+            ) {
+                items(NoteName.values()) { noteName ->
+                    Button(
+                        onClick = { onClickButton(noteName) },
+                        enabled = noteName != selectedNoteName,
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
+                            .height(48.dp),
+                    ) {
+                        Text(text = noteName.text)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(widthDp = 300)
+@Preview(widthDp = 300, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(widthDp = 500)
+@Preview(widthDp = 500, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun NoteNameSectionPreview() {
+    MusAppTheme {
+        NoteNameSection(selectedNoteName = NoteName.C)
+    }
+}
