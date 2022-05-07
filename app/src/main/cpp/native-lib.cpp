eed1f91d9f77ea07ca0ab70144bdb78f22ccee1f
Go Shibata <warahiko@gmail.com>
2022-05-07 11:55:30 +0900

    デフォルトの関数削除

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,5 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_io_github_warahiko_mus_ui_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C" {
 }
