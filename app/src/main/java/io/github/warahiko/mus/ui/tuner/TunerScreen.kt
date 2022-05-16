76a9a44e96d14562aafb8ea8dcbc0e7846970b42
Go Shibata <warahiko@gmail.com>
2022-05-16 22:40:19 +0900

    TunerScreen の画面実装

--- app/src/main/java/io/github/warahiko/mus/ui/tuner/TunerScreen.kt
+++ app/src/main/java/io/github/warahiko/mus/ui/tuner/TunerScreen.kt
@@ -1,17 +1,27 @@
 package io.github.warahiko.mus.ui.tuner
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.github.warahiko.mus.R
 import io.github.warahiko.mus.ui.component.MusMainTopBar
 import io.github.warahiko.mus.ui.theme.MusAppTheme
@@ -40,13 +50,43 @@ fun TunerScreen(
         Column(
             modifier = Modifier
                 .padding(contentPadding)
+                .padding(horizontal = 20.dp)
+                .padding(top = 12.dp, bottom = 24.dp)
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
-                text = "Tuner",
+                text = stringResource(id = R.string.tuner_a4_frequency, 442),
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.fillMaxWidth(),
             )
+            Spacer(modifier = Modifier.height(60.dp))
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                Text(
+                    text = stringResource(id = R.string.tuner_cent, "+21"),
+                    style = MaterialTheme.typography.headlineLarge,
+                )
+            }
+            TunerMeter(
+                value = 21f,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.Bottom,
+            ) {
+                Text(
+                    text = "C#",
+                    fontSize = 128.sp,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.displayLarge,
+                )
+                Text(
+                    text = "4",
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.displayMedium,
+                )
+            }
         }
     }
 }
